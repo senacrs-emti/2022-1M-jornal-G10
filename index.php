86022ff91db605e08f41a4a94f34475b69d01d44
Perdop <pedrohoxsantana@gmail.com>
2022-10-07 08:20:19 -0300

    Arquivos de base 0.2

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
-include_once './includes/_header.php'
+include_once './include/_header.php';
 ?>
 <main>
 
 </main>
 
 <?php 
-include_once './include/_aside.php'
-include_once './include/footer.php'
+include_once './include/_aside.php';
+include_once './include/_footer.php';
 ?>
