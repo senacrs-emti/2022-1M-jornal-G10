75a7c629a3e36e2a6f37368a71b204cb17f245ad
Enzo <ebdalmoro@gmail.com>
2022-10-07 10:02:38 -0300

    FooterUpdate

---
+++ include/_header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="./assets/js/script.js" defer></script>
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>Technews</title>
+</head>
+<header>
+    <nav>
+        <a href="">Lorem</a>
+        <span class="navseparationbar"></span>
+        <a href="">Lorem</a>
+        <span class="navseparationbar"></span>
+        <a href="">Lorem</a>
+        <span class="navseparationbar"></span>
+        <a href="">Lorem</a>
+        <span class="navseparationbar"></span>
+        <a href="">Lorem</a>
+        <span class="navseparationbar"></span>
+        <a href="">Lorem</a>
+    </nav>
+</header>
+
--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
-include_once './includes/_header.php'
+
+include_once './include/_header.php';
 ?>
 <main>
 
 </main>
 
 <?php 
-include_once './include/_aside.php'
-include_once './include/footer.php'
+
+include_once './include/_aside.php';
+include_once './include/_footer.php';
+
 ?>
