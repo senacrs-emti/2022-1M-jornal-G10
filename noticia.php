4e250dc604d67eefe2f4c3b035a01f1e9b0f95ea
picanha477 <108525550+picanha477@users.noreply.github.com>
2022-10-21 09:11:22 -0300

    Noticia update

--- noticia.php
+++ noticia.php
@@ -6,7 +6,7 @@ include_once './include/_header.php';
 <main>
     <div class="NoticiaTopo">
         <img src="./img/bolso-PRO.jpg" alt="" class="PropagandaNoticia">
-        <h1></h1>
+        <h1>Título da Notícia</h1>
     </div>
     <div class="corpo">
         <div class="ArtigoNoticia">
