e7bd2526abdc399e1f3d4ba5aa7ffe1731219b02
Perdop <pedrohoxsantana@gmail.com>
2022-10-21 10:04:51 -0300

    aside 0.2

--- include/_aside.php
+++ include/_aside.php
@@ -1,7 +1,16 @@
 <aside>
-    <span class="divbar"></span>
+    <span class="divbaraside"></span>
     <div class="MaisLidos">
         <h1>Mais Lidos</h1>
+        <div class="ArtigosMaisLidos">
+            <span class="box1MaisLidos">
+                <h3>Lorem lore lor loren</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus similique accusamus nam, incidunt temporibus a, optio ducimus qui eligendi molestiae nostrum eveniet? Consequatur sunt nemo asperiores. Amet adipisci ullam nulla.</p>
+            </span>
+            <span class="box2MaisLidos">
+                <img src="" alt="">
+            </span>
+        </div>
     </div>
     <div class="propaganda"></div>
     <div class="Relacionados"></div>
