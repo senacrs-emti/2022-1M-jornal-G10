919cc5f289dcde6974b1eb6d13fa2b93352c29b3
Perdop <pedrohoxsantana@gmail.com>
2022-10-27 08:10:54 -0300

    Aside 0.3

--- include/_aside.php
+++ include/_aside.php
@@ -5,7 +5,16 @@
         <div class="ArtigosMaisLidos">
             <span class="box1MaisLidos">
                 <h3>Lorem lore lor loren</h3>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus similique accusamus nam, incidunt temporibus a, optio ducimus qui eligendi molestiae nostrum eveniet? Consequatur sunt nemo asperiores. Amet adipisci ullam nulla.</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.ojnvwl  </p>
+            </span>
+            <span class="box2MaisLidos">
+                <img src="" alt="">
+            </span>
+        </div>
+        <div class="ArtigosMaisLidos">
+            <span class="box1MaisLidos">
+                <h3>Lorem lore lor loren</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.ojnvwl  </p>
             </span>
             <span class="box2MaisLidos">
                 <img src="" alt="">
