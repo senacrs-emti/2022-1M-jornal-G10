2fe26b553782f7899dc36b7ed27b661c364f3125
Enzo Braga <ebdalmoro@gmail.com>
2022-11-24 21:33:08 -0300

    Update footer

--- include/_footer.php
+++ include/_footer.php
@@ -3,10 +3,10 @@
                 <div class="logospace">
                     <img src="./img/Logo.png" alt="logofooter" class="Logofooter">
                     <p class="textfooter">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis autem unde sapiente consequatur eum sunt voluptatum neque tenetur odit. Magni mollitia voluptate, itaque nihil eligendi tempore tenetur optio. Architecto, quibusdam! Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                    <a href="mailto:ebdalmoro@gmail.com" class="email"><img src="./img/email2.png" alt="email"  height = "45vh" width = "50vw"></a>
-                    <a href="" class="face"><img src="./img/facebook.png" alt="face" height = "45vh" width = "50vw"></a>
-                    <a href="" class="insta"><img src="./img/instagram.png" alt="insta" height = "45vh" width = "50vw"></a>
-                    <a href="" class="tt"><img src="./img/twitter.png" alt="tt" height = "45vh" width = "50vw"></a>
+                    <a href="mailto:ebdalmoro@gmail.com" class="email"><img src="./img/email2.png" alt="email"  height = "30vh" width = "35vw"></a>
+                    <a href="" class="face"><img src="./img/facebook.png" alt="face" height = "35vh" width = "35vw"></a>
+                    <a href="" class="insta"><img src="./img/instagram.png" alt="insta" height = "33vh" width = "35vw"></a>
+                    <a href="" class="tt"><img src="./img/twitter.png" alt="tt" height = "33vh" width = "35vw"></a>
                 </div>
             </div>
             <div class="rightsidefooter">
