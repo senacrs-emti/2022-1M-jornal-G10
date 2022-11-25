8661e0ca2bb5e29bafa4c79b4f930bfed7ecd0c2
viniross <viniciusrossws@gmail.com>
2022-11-25 09:59:47 -0300

    noticia.php2

--- noticia.php
+++ noticia.php
@@ -7,7 +7,14 @@
         <h1 class="titulonoticia">Titulo da noticia</h1>
         <div class="divbarnotice"></div>
         <div class="box2noticia"></div>
-
+        <figcaption>📷(crédito: / )</figcaption>
+        <div class="texto">
+        <p class="pnoticia1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis nesciunt iusto accusantium est. Impedit ex autem quis! Nemo libero iure laborum iste nihil mollitia consequuntur, necessitatibus autem similique voluptas aliquam.</p>
+        <p class="pnoticia2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, fugit odio quae adipisci debitis ab, nulla ipsum temporibus perspiciatis facilis soluta exercitationem nostrum dolorem voluptatum, doloremque consectetur et consequatur inventore!</p>
+        <p class="pnoticia2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo sapiente expedita fugit accusantium maxime totam consectetur nisi ullam! Dolore quas ab voluptatum, porro pariatur repudiandae amet. Dignissimos qui fugit doloremque.</p>
+        <p class="pnoticia2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, fugit odio quae adipisci debitis ab, nulla ipsum temporibus perspiciatis facilis soluta exercitationem nostrum dolorem voluptatum, doloremque consectetur et consequatur inventore!</p>
+        <p class="pnoticia2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo sapiente expedita fugit accusantium maxime totam consectetur nisi ullam! Dolore quas ab voluptatum, porro pariatur repudiandae amet. Dignissimos qui fugit doloremque.</p>
+        </div>
     </main>
     <?php 
         include_once './include/_footer.php';
