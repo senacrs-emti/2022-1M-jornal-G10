4b92e9837b16c2bec0a6833ecc7c805fe04ec427
Enzo Braga <ebdalmoro@gmail.com>
2022-11-28 17:03:23 -0300

    Last Update

--- include/_footer.php
+++ include/_footer.php
@@ -1,5 +1,24 @@
         <footer>
-            
+            <div class="logobox">
+                <img src="./img/LogoNewColor.png" alt="logo" class="logofooter">
+                <p class="txtfooter">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum alias nulla facere nobis consectetur atque amet aliquid quo excepturi deserunt quia eius nam commodi, saepe quas placeat. Expedita, saepe rerum. Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                <a href="" class="ctt_footer"><img src="./img/twitter.png" alt="tt" height="12%"></a>
+                <a href="" class="ctt_footer"><img src="./img/instagram.png" alt="insta" height="12%"></a>
+                <a href="" class="ctt_footer"><img src="./img/email2.png" alt="email" height="12%"></a>
+                <a href="" class="ctt_footer"><img src="./img/facebook.png" alt="face" height="12%"></a>
+            </div>
+            <div class="sobre_footer">
+                <h4>Alguns Links</h4>
+                <ul>
+                    <li class="list_lik"><a href="">F.A.Q</a></li>
+                    <li class="list_lik"><a href="">Cookies politcy</a></li>
+                    <li class="list_lik"><a href="">Terms of Service</a></li>
+                    <li class="list_lik"><a href="">Support</a></li>
+                </ul>
+                <h4 class="news_footer">NEWSLETTER</h4>
+                <input type="text" name="" id="inpt_footer" placeholder="Your Email Adress">
+                <button class="sub_footer">Subscribe</button>
+            </div>
         </footer>
     </body>
 </html>
