1a18b95b47c6b18d53e251c490cda0655fa83057
Raphael <100046113+RaphaelBarcelos@users.noreply.github.com>
2022-11-29 20:05:21 -0300

    editorial pronto

--- aplicativos.php
+++ aplicativos.php
@@ -6,34 +6,34 @@ include_once './include/_header.php';
     <main>
         <div class="news-group1">
             <span>
-                <img src="./img/div.jpg" alt="">
-                <img src="./img/div.jpg" alt="">
+                <a href=""><img src="./img/div.jpg" alt=""></a>
+                <a href=""><img src="./img/div.jpg" alt=""></a>
             </span>
         </div>
         <div class="news-group2">
             <span>
-                <img src="./img/div.jpg" alt="">
+                <a href=""><img src="./img/div.jpg" alt=""></a>
             </span>
         </div>
         <article style="width: 65%;">
             <div class="news-group3">
                 <div class="group3-img"></div>
                 <span>
-                    <h2>Titulo</h2>
+                    <a href=""><h2>Titulo</h2></a>
                     <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis consequatur fuga mollitia quas amet, cupiditate molestias impedit? Sed obcaecati sapiente fugiat quaerat placeat ipsa iusto! Odit fuga incidunt adipisci commodi!</h3>
                 </span>
             </div>
                 <div class="news-group4">
                 <div class="group4-img"></div>
                 <span>
-                    <h2>Titulo</h2>
+                    <a href=""><h2>Titulo</h2></a>
                     <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis consequatur fuga mollitia quas amet, cupiditate molestias impedit? Sed obcaecati sapiente fugiat quaerat placeat ipsa iusto! Odit fuga incidunt adipisci commodi!</h3>
                 </span>
             </div>
                 <div class="news-group5">
                 <div class="group5-img"></div>
                 <span>
-                    <h2>Titulo</h2>
+                    <a href=""><h2>Titulo</h2></a>
                     <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis consequatur fuga mollitia quas amet, cupiditate molestias impedit? Sed obcaecati sapiente fugiat quaerat placeat ipsa iusto! Odit fuga incidunt adipisci commodi!</h3>
                 </span>
             </div>
