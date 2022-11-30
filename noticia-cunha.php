5a5fea32d04f255bb81ce9c9a1114018c6bfb2d8
Matheus Cunha <matheusbabinski123@gmail.com>
2022-11-29 22:14:10 -0300

    Create noticia-cunha.php

---
+++ noticia-cunha.php
@@ -0,0 +1,22 @@
+<?php 
+        include_once './include/_header.php';
+    ?>
+    <main>
+        <div class="box1noticia"></div>
+            <h1 class="titulonoticia">Streamer sua nova profissão do futuro</h1>
+            <hr class="divbarnotice"></hr>
+            <div class="box2noticia"></div>
+            <figcaption>📷(crédito: / )</figcaption>
+        <div class="texto_not">
+            <p class="pnoticia1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis nesciunt iusto accusantium est. Impedit ex autem quis! Nemo libero iure laborum iste nihil mollitia consequuntur, necessitatibus autem similique voluptas aliquam. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla quasi quia quos, doloribus at velit distinctio nobis delectus qui nam similique pariatur ab provident aliquam. Minima optio nam ipsum architecto.</p>
+            <p class="pnoticia2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, fugit odio quae adipisci debitis ab, nulla ipsum temporibus perspiciatis facilis soluta exercitationem nostrum dolorem voluptatum, doloremque consectetur et consequatur inventore! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci, repellendus. Alias, at excepturi omnis ullam tempore reiciendis labore doloremque illo molestias. Dicta doloremque asperiores nostrum inventore quo aliquid vitae id!</p>
+            <p class="pnoticia2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo sapiente expedita fugit accusantium maxime totam consectetur nisi ullam! Dolore quas ab voluptatum, porro pariatur repudiandae amet. Dignissimos qui fugit doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam libero reprehenderit labore nam blanditiis sunt quod tempora possimus alias magnam quis totam dignissimos nostrum voluptatem, aliquam tempore. Dolores, commodi accusamus.</p>
+            <p class="pnoticia2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, fugit odio quae adipisci debitis ab, nulla ipsum temporibus perspiciatis facilis soluta exercitationem nostrum dolorem voluptatum, doloremque consectetur et consequatur inventore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut quae nulla odit laboriosam. Ad tempore natus quaerat fugit, expedita porro qui cum modi reiciendis. Corporis quas ipsum hic error maiores.</p>
+            <p class="pnoticia2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo sapiente expedita fugit accusantium maxime totam consectetur nisi ullam! Dolore quas ab voluptatum, porro pariatur repudiandae amet. Dignissimos qui fugit doloremque. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod unde ducimus modi, fugiat, exercitationem quos mollitia minus culpa labore, atque neque a veritatis soluta assumenda. Ducimus harum quas reprehenderit omnis.</p>
+</div>
+    </main>
+<div class="IndexFooter">
+    <?php 
+        include_once './include/_footer.php';
+    ?>
+<div>
