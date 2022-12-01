f9dec4ded107d9567dfe793e6c412a05c6da19b3
Vinícius Ross Weingaertner dos Santos <viniciusrossws@gmail.com>
2022-12-01 18:35:11 -0300

    noticia vini

---
+++ noticia-vini.php
@@ -0,0 +1,52 @@
+<?php 
+        include_once './include/_header.php';
+    ?>
+    <main>
+        <div class="NoticiaLayoutConfiguration">
+            <div class="NoticiaLeft">        
+                <div class="box1noticia"></div>
+                <h1 class="titulonoticia">IEM Major Rio 2022 e a saga dos times brasileiros </h1>
+                <div class="texto_not">
+            <p class="pnoticia2">IEM Major Rio 2022 foi o primeiro campeonato mundial de CS:GO no Brasil. CS:GO é um jogo de fps 5 contra 5, onde tem os terroristas(TR) e os contraterroristas(CT), para vencer é preciso que a bomba detone no caso dos terroristas e os contraterroristas precisam desarma-la. Em cada partida ganha em quem chegar em 16 pontos primeiro, mas caso tenha um empate em 15x15 é jogado mais 6 rounds e quem chegar em 19 pontos primeiro ganha, caso empate de novo o mesmo formato acontece quantas vezes for necessário.</p>
+            <h3>Classificação</h3>
+            <p class="pnoticia2">O IEM Major Rio 2022 tem 4 estágios, as qualificações, onde todos os times jogam contra todos de sua região e de lá se classificam 6 times, podendo ter alguns times que se classificam direto para o Legends Stage. Quando já estamos no major começa a ficar um pouco mais complicado, para os times que começaram no Challengers Stage, precisam ganhar 3 partidas para se classificar e quem perder 3 está fora do campeonato, se classificando 8 times, a mesma coisa acontece no Legends Stage. O último estágio, Champions Stage, passam 8 times para disputar quem será o campeão, numa fase de mata-mata, começando em Quartas de finais.</p>
+            <h3>Imperial Esports</h3>
+            <p class="pnoticia2">A queridinha dos Brasileiros, mais conhecida como The Last Dance. A Imperial é um time que foi formado no início desse ano com o intuito de ser o último ano de alguns jogadores que ganharam o major para o Brasil iriam se aposentar como o Gabriel "Fallen" Toledo. Infelizmente a nossa Imperial não conseguiu passar nem para a Segunda fase do Major, sendo o primeiro time eliminado do Challengers Stage e perdendo de um 0-3.  </p>
+            <div class="NoticiaMainImg">
+                    <img src="./img/imperial.jpg" alt="">
+                </div>
+                <hr class="divbarnotice"></hr>
+                <div class="box2noticia"></div>
+                <figcaption>📷(crédito: <a href="https://twitter.com/brcho_"> Josip Brtan/HLTV</a> )</figcaption>
+            <h3>00 Nation</h3>
+            <p class="pnoticia2">O Segundo time eliminado, tendo jogadores mais conhecidos pela comunidade como, Marcelo "Coldzera" David e Epitácio "Taco" de Melo, assim como a nossa queridíssima Imperial eles acabaram sendo eliminados no Challengers Stage, sendo a segunda eliminada da competição perdendo de 0-3 também.</p>
+            <div class="NoticiaMainImg">
+                    <img src="./img/taco.jpg" alt="">
+                </div>
+                <hr class="divbarnotice"></hr>
+                <div class="box2noticia"></div>
+                <figcaption>📷(crédito: <a href="https://twitter.com/brcho_">Josip Brtan/HLTV</a> )</figcaption>
+                <h3>Furia Esports</h3>
+            <p class="pnoticia2">O terceiro e último time brasileiro que participou do IEM Major Rio 2022, foi a Furia. A Furia mantém 3 jogadores desde 2018 na sua composição. A Furia felizmente conseguiu se classificar para o Legends Stage, terminando a primeira fase da competição com um 3-1 e sendo a esperança dos brasileiros no campeonato. Já no Legends Stage eles conseguiram fazer um 3-0, passando para a próxima fase com facilidade e indo assim para o mata-mata. Nas Quartas de final do mata-mata a Furia conseguiu ganhar de virada por 2x1 da poderosíssima NAVI, time que era considerado um dos favoritos a ganhar o campeonato por muitos. A Furia foi o único time brasileiro a passar para uma semi-final de Major desde 2019, quando a MIBR tinha se classificado, mas para a tristeza dos brasileiros infelizmente caímos nas semis com uma virada de 2x1 contra a Heroic, futura Vice-Campeã do Campeonato. </p>
+            <div class="NoticiaMainImg">
+                    <img src="./img/furia.jpg" alt="">
+                </div>
+                <hr class="divbarnotice"></hr>
+                <div class="box2noticia"></div>
+                <figcaption>📷(crédito: <a href="https://twitter.com/brcho_">Josip Brtan/HLTV</a> )</figcaption>
+                </div>
+            </div>
+            <div class="NoticiaRight">
+                <div class="Aside">
+                    <?php
+                    include_once './include/_aside.php';
+                    ?>
+                </div>
+            </div>
+        </div>
+    </main>
+<div class="IndexFooter">
+    <?php 
+        include_once './include/_footer.php';
+    ?>
+<div>
